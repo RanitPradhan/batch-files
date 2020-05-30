d67badadcf33540e028250a9ac90f7b5a989525c
ViperRanit <ViperRanit>
2020-05-31 00:44:43 +0530

    Hacker Sketch

---
+++ HackBgnd.bat
@@ -0,0 +1,58 @@
+@echo off
+color 0a
+title Hacker Batch
+echo Hacker Batch
+echo
+                                                                                                  
+echo                   .:+oyyyyyso/-.                                    .-/+oyyyyso+:`                 
+echo                `:sdmmdmmdddhdddhho:.                            ./shddhyyhddmddmdyo:               
+echo               -ymdmmmmmmmmmdhyo///+++-`                      `-+++//+osyhddmmddmmddmy-             
+echo     /dmmmmmmdmmmmmmmmdhhho:.``                      ``.:ohdmmmhdmmmmmmmmhdmmd/            
+echo             /mmmdhhhhhdmmmmddmdhdmmmds:`                    `:sdmmmddmdhdmmmmhhhhyyddmm:           
+echo            `o/:..`````..-/oshmmdmmmmmmmh+`                .ohddmmmmmmmmhyo/-..`````..:++`          
+echo                    `-+sdmmmmmmmdy/              +dmdhdmmmmdy+-`                           
+echo                                 .+hmmmmmso+             `osymddmmy/.                               
+echo                                   `:ydmNy`                `hmhhs-`                                 
+echo                        ``...` `      -smmo                omdo.      ` `...``                      
+echo                    `-/osssso:`o+++:-`  :yd.              :my-  `-/+oso :oossss+-                   
+echo          `        -yy+::/+osyysooosyys/``/o .          . s/``/syyssssysooo+/:/ody-        `        
+echo         `:`    .ods/shmmNNmNNmdmmmdhymd- . /         `/ . :ddyhdmNmmNmdmmmmmhs/sdo`    `:`        
+echo           .//--odm++ddmNmNNmNNNdmNmNmdmNy   s.        -o   hmhhmmmNNNNmhdmmmNmmd/+dh+--//`       
+echo         `-/ydddmdhs+/+ossyyyyysssosyyhs.   h-        :h   -syysoossyysssysso++ooyhdddds/-`        
+echo         `:/::-.``.:oyhhdhhhh/ ysyhhy+:`    .m:        /m.    .:oyhhhhy /yhhddddho:.``.-::+:`       
+echo        `--            `.----. -..`         sm:        /mo         `.-- .----.`           `--       
+echo                                           :NN:        /Nm-                                         
+echo                                          .dNm-        :NNh.                                        
+echo                                         -hNNN-        :NNdh.                                       
+echo                                        /mhhNm.        -NNdNm/                                      
+echo                                      .ymo`-mm`        `NN..sms`                                    
+echo                                 `   +mo`  /Nh          dN:  .od/   `                               
+echo          .`                  `/sms +y`    oNo          oN+    .y/ ymy/`                  ``        
+echo          `h+-           `./ohNms/` y`     sN-          :N+     .s `/ymNho:.            -od`        
+echo           /dddooo+ooooshyys+:.     --     -m+          oN-     -.     .:+syyhysso++oosmNN/         
+echo            sdms`ommNs.``               /hs/:shs+////+yhs:+hd/               ``.ymdd+`yNNs          
+echo            `dNm+ /mNNh:                 `-:-:--:++++/--:-:-`                 :dNNd: oNNd`          
+echo             .dmm/ -dNdms.                `ommmmhyhyyydmmm+`                .ymNNh. +mmm.           
+echo              -dNN: `ymNNm/              :hNNNNNmdyydmNNNNmh:             `+mNNNs` /mNd.            
+echo               -dNm/ `/dNmmh:-----:://+sdmddNmNNs.  .yNNNNNNNho+//::-----:hmmNd/` /dNh.             
+echo                .hmNs` -hmNNmNNdhdmmNNmNNmddNmd/     `/dNNmNNNmNNNNNddNdhdNmNh- `ohdy.              
+echo                 `yNNd:``/hNNNNNmNNNNNNNNNmNNs.        .yNNNNNmmNNNNNNNNmNmh/``/dNms`               
+echo                  `+NNy+:``..----------:yysyy+++++++//++oyyyys:-----------.``:+dNm/                 
+echo                    -dms-//-`          `.--://///++///////:-..           `-//-sNh-                  
+echo                     `+ds``-:/-.`                                    `.-::-`.yd+`                   
+echo                       -hh.   `````                                `.`.`   -hh-                     
+echo                        `+d:                 `-::::---:-`                 /d+`                      
+echo                         -s+`               `/mmNNmddd:                `os.                        
+echo                            :o.                .mNNNmd.                -o:                          
+echo                             `:-               `dmNNmd                -:`                           
+echo                               ``              +mmNNmN/              ``                             
+echo                                               dNNNNdmh                                             
+echo                                               NmNNmddm                                             
+echo                                               mNNNmdmd                                             
+echo                                               hmmNmddy                                             
+echo                                               +mNNmdm/                                             
+echo                                               .mmNmdd`                                             
+echo                                               hmNmds                                              
+echo                                                /NNNm:                                              
+echo                                                 +hh/   
+pause > nul                                            
